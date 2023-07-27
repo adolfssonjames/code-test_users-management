deb99121cd8afb9f84c3e5645ddd490714adb887
James <adolfssonjames@gmail.com>
2023-07-27 15:55:17 +0200

    added form and posting formdata to api

--- kodprov-james/src/components/form/Form.tsx
+++ kodprov-james/src/components/form/Form.tsx
@@ -1,3 +1,71 @@
+import { useState } from "react";
+import { IFormInputs } from "../../types/formTypes";
+import { postData } from "../../api/requests";
 export default function Form() {
-  return <div>Form</div>;
+  const [formData, setFormData] = useState<IFormInputs>({
+    userId: 1,
+    title: "",
+    body: "",
+  });
+
+  const handleChange = (
+    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const { name, value } = event.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    postData(formData)
+      .then((response) => {
+        // Handle the response from the API
+        console.log("Response from the API:", response.data);
+      })
+      .catch((error) => {
+        // Handle any errors that occurred during the API call
+        console.error("Error:", error);
+        alert("An error occurred. Please try again later.");
+      });
+  };
+  return (
+    <div>
+      <h2>Login</h2>
+
+      <section>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="title">Username</label>
+          <div>
+            <input
+              type="text"
+              placeholder="Your Username (required)"
+              id="title"
+              name="title"
+              value={formData.title}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <label htmlFor="body">Password</label>
+          <div>
+            <input
+              type="password"
+              placeholder="min 6 characters (required)"
+              id="body"
+              name="body"
+              value={formData.body}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div>
+            <button type="submit">Submit</button>
+          </div>
+        </form>
+      </section>
+    </div>
+  );
 }
